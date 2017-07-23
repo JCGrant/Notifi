2604fa63413c0c62984c22ec36423d351c7f6cfe
JCGrant <jamescolin.grant@gmail.com>
2017-07-23 22:56:04 +0100

    Send SMS data to a server

--- app/src/main/java/com/jcgrant/notifi/SmsReceiver.java
+++ app/src/main/java/com/jcgrant/notifi/SmsReceiver.java
@@ -7,7 +7,19 @@ import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SmsReceiver extends BroadcastReceiver {
+
+    private static final String URL = "http://192.168.1.114:9090";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -16,10 +28,32 @@ public class SmsReceiver extends BroadcastReceiver {
             byte[][] pdus = (byte[][]) bundle.get("pdus");
             for (int i = 0; i < pdus.length; i++) {
                 SmsMessage message = SmsMessage.createFromPdu(pdus[i]);
-                String msgFrom = message.getOriginatingAddress();
-                String msgBody = message.getMessageBody();
-                Log.d("SmsReceiver", msgFrom + " : " + msgBody);
+                JSONObject json = new JSONObject();
+                try {
+                    json.put("from", message.getOriginatingAddress());
+                    json.put("msg", message.getMessageBody());
+                } catch (JSONException e) {
+                    Log.e("SmsReceiver", e.toString());
+                }
+                sendJson(context, URL, json);
             }
         }
+    }
+
+    private void sendJson(Context context, String url, JSONObject json) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, url, json,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d("SmsReceiver", response.toString());
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("SmsReceiver", error.toString());
+                }
+            });
+        queue.add(stringRequest);
     }
 }
